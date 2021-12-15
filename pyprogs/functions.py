e07a49e88154d005a45ce8e3af78c24d28f566a1
Sayad Pervez <pervez2504@gmail.com>
2021-12-15 18:54:19 +0530

    clearFreeSpace bug fix commit

--- pyprogs/functions.py
+++ pyprogs/functions.py
@@ -221,3 +221,7 @@ def clearFreeSpace():
     li = os.listdir()
     for _ in li:
         os.remove(f"./{_}")
+    os.chdir("./../SVG/")
+    li = os.listdir()
+    for _ in li:
+        os.remove(f"./{_}")
