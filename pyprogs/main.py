7124887423262981f259888ffc8843ff1ac2b0b1
Sayad Pervez <pervez2504@gmail.com>
2021-12-16 19:31:22 +0530

    All 4 algorithms basic testing completed

--- pyprogs/main.py
+++ pyprogs/main.py
@@ -9,6 +9,10 @@ sheet=CutSheet(30,10,0,"abcdefgh1")
 circle = Circle(50,"qwerty1")
 cone = Cone(30,10,0,"zxcvb1")
 canva=Canvas(1920,1080)
+li = [sheet,circle,cone]
 
-out = algorithm1.run(canva,[sheet,circle,cone],log_=True)
+out = algorithm1.run(canva,li,log_=True,constCompute=100)
 a2p(out).show()
+
+for _ in li:
+    print(_.low_res_pos)
