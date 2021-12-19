d6be1439693bb2a6f32b2dc3d6e9443e6c4d44ec
Sayad Pervez <pervez2504@gmail.com>
2021-12-19 20:30:59 +0530

    cors updated policy

--- pyprogs/pysock.py
+++ pyprogs/pysock.py
@@ -5,15 +5,14 @@ import socketio
 x = sys.argv[1]
 
 sio = socketio.Client()
+sio.connect("http://localhost:3000")
 
 @sio.event()
 def connect():
-    print("Connected")
+    print("Connected -pythonsocket")
     sio.emit("_mod.js",x)
 
 @sio.event()
 def die(data):
     sio.disconnect()
     exit()
-
-sio.connect("http://localhost:3000")
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*:*"
+  }
+});
 
 app.use(express.static('./front-end'));
 
