ff5bfa6f2a53f495cebb2e949f630ddeb6f557ca
Sayad Pervez <pervez2504@gmail.com>
2021-12-19 10:55:00 +0530

    Completion intimation

--- front-end/comms.js
+++ front-end/comms.js
@@ -2,6 +2,9 @@ var socket = io();
 
 socket.on("Process Confirmation",(status)=>{
     toaster("File creation "+String(status),"white-text green darken-2 ");
+    document.getElementById("info_butt").innerHTML = `<strong>Process Completed ...</strong>`;
+    document.getElementById("info_butt").classList.remove("red");
+    document.getElementById("info_butt").classList.add("green");
 });
 
 socket.on("notification",(data)=>{
